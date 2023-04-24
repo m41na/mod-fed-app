5df647ddc950170fbc161a344e32255e52bbd799
Maina, Stephen <m41na@yahoo.com>
2023-04-23 22:30:33 -0500

    Ading header to container module

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from '../components/MarketingApp';
-console.log('App ling');
+import Header from './components/Header';
 
 function App() {
     return (
-        <MarketingApp />
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 }
 
