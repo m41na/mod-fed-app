9c39e444dfbd3a9e18e109c390ec0474ee1d95ad
Maina, Stephen <m41na@yahoo.com>
2023-04-24 08:18:56 -0500

    adding classname generator for randomness

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
 import MarketingApp from '../components/MarketingApp';
 import Header from './components/Header';
 
+const genClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
+
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={genClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
 
